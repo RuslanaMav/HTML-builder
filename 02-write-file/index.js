f8109c540471f3d8d3d8540dc865be75c6ffe118
RuslanaMav <116889933+RuslanaMav@users.noreply.github.com>
2023-05-10 19:29:42 +0500

    change index.js 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,15 +3,19 @@ const path = require('path');
 const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 const { stdin, stdout } = process;
 stdout.write('Enter text:\n');
-stdin.on('data', data => {
-    if (data.toString() !== "exit\r\n") output.write(data);
-    else {
-        stdout.write("Goodbye!")
+
+stdin.on('data', (data) => {
+    if (data.toString().trim() === 'exit') {
+        stdout.write('Goodbye!');
         process.exit();
     }
-});
+    else{
+      output.write(data.toString());
+    }
+  });
 
 process.on('SIGINT', () => {
     stdout.write('Goodbye!');
     process.exit();
 });
+
